37243ac623b96a402845cb9c976b5d716e2d17d7
Recepoğlu <73506878+erecepoglu@users.noreply.github.com>
2026-01-11 00:45:20 +0000

    scanf issues

--- main.c
+++ main.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-    float *num;
+    float *num1,*num2;
     printf("Enter a number: ");
-    scanf("%f",num);
-    printf("Number is %f ,the address is %p\n",*num,num);
-    printf("Number is %d ,the address is %p\n",(int)&num,num);
+    scanf("%f",num1);
+    /*scanf breaks if you enter 2 numbers separated with white space
+        and the second scanf if will read the second number immediately
+    */
+    scanf("%f",num2);
+    printf("Number1 is %f ,the address is %p\n",*num1,num1);
+    printf("Number2 is %f ,the address is %p\n",*num2,num2);
+
+    //printf("Number is %d ,the address is %p\n",(int)&num,num);
 }
