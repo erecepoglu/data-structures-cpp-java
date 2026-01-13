09ee549ff49dec3e2f2fb5b3fb96fd890ed713d8
Recepoğlu <73506878+erecepoglu@users.noreply.github.com>
2026-01-13 19:18:21 +0000

    creating heap

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -9,6 +9,21 @@ void insert(int arr[], int n){
     }
     arr[i] = temp;
 }
+void createHeap(int arr[],int n){
+    for(int i = 2; i <= n; i++){
+        insert(arr,i);
+    }
+}
 int main(){
-
+    int array[] = {0,10,20,30,25,5,40,35};
+    int size = sizeof(array)/sizeof(array[0]);
+    for(int i = 0; i < size; i++){
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+    createHeap(array,size);
+    for(int i = 0; i < size; i++){
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
 }
