cd22dde4f452af881238c50787e28960e4a4f7a2
Recepoğlu <73506878+erecepoglu@users.noreply.github.com>
2026-01-14 15:20:48 +0000

    Adding delete heap function

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 void insert(int arr[], int n){
     //O(log_n)
@@ -14,6 +15,28 @@ void createHeap(int arr[],int n){
         insert(arr,i);
     }
 }
+void deleteHeap(int arr[], int n){
+    int x,i,j;
+    x = arr[n];
+    arr[1] = arr[n];
+    i = 1, j = 2 * i;
+    while(j <    n -1){
+        if(arr[j+1] > arr[j]){
+            j = j+1;
+        }
+        if(arr[i] > arr[j]){
+            //std::swap(arr[i],arr[j]);
+            swap(&arr[i],&arr[j]);
+            i = j;
+            j *= 2;
+        }else break;
+    }
+}
+void swap(int *num1, int *num2){
+    *num1 ^= *num2;
+    *num2 ^= *num1;
+    *num1 ^= *num2;
+}
 int main(){
     int array[] = {0,10,20,30,25,5,40,35};
     int size = sizeof(array)/sizeof(array[0]);
@@ -26,4 +49,12 @@ int main(){
         std::cout << array[i] << " ";
     }
     std::cout << std::endl;
+    int num1 = 2, num2 = 3;
+    std::cout << "Before swapping using bitwise: \n";
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
+    swap(&num1,&num2);
+    std::cout << "After swapping using bitwise: \n";
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
 }
